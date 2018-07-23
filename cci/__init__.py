32829050b81512ec5405100358da2a7bcfb654ad
Ellen Marie Dash <me@duckie.co>
2018-07-23 06:21:04 -0400

    *flails*

--- cci/__init__.py
+++ cci/__init__.py
@@ -5,12 +5,16 @@ import shlex
 from subprocess import Popen, PIPE
 import sys
 
-def skip_shebang(f):
-    line = f.readline()
-    if line.startswith("#!"):
-        f.seek(len(line))
+def get_headers(f):
+    shebang = f.readline()
+    args_str = f.readline()
+    if args_str.startswith("# cci:"):
+        args = args_str.split("# cci:")[1]
+        f.seek(len(shebang) + len(args_str))
     else:
-        f.seek(0)
+        args = ""
+        f.seek(len(shebang))
+    return (shebang, args)
 
 def pipe(cmds, stdin, stdout):
     procs = []
@@ -25,22 +29,23 @@ def pipe(cmds, stdin, stdout):
 
 def main(argv=None):
     """
-    cci isn't built for being called from the commandline.
-    It's designed for being put in the shebang line of C files.
+    cci is designed for being put in the shebang line of C files.
+    Due to limitations in how shebang lines work, arguments to
+    Clang are on the line immediately after the shebang line, and
+    that line must start with "# cci:".
 
     For example, save the following to "hello-world.c":
 
-        #!cci -std=c11 -Wall -pedantic-errors
+        #!/usr/bin/env cci
+        # cci: -std=c11 -Wall -pedantic-errors
         #include <stdio.h>
         int main() {
-            printf("Hello, world!");
+            printf("Hello, world!\n")
             return 0;
         }
 
     Now, mark it as executable (on *nix, `chmod +x hello-world.c`),
     then run `./hello-world.c`.
-
-    It should run without issue, _without creating any new files_.
 
     DEBUGGING
         If you include the argument "-###" in the arguments, it will be
@@ -49,15 +54,16 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    if any(map(lambda x: "-h" in x, argv)) or len(argv) != 2:
-            print(inspect.getdoc(main))
-            exit(1)
+    if any(map(lambda x: x == "-h" or x == "--help", argv)) or len(argv) != 1:
+        # Print help text.
+        print(inspect.getdoc(main))
+        exit(1)
+    else:
+        # We got a file name only.
+        f = open(argv[0])
+        _, header_argv = get_headers(f)
 
-    args = shlex.split(argv[0])
-    filename = argv[1]
-
-    f = open(filename)
-    skip_shebang(f)
+    args = shlex.split(header_argv)
 
     clang = ["clang", "-xc", *args, "-", "-S", "-emit-llvm", "-o", "-"]
     lli   = ["lli", "-"]
--- examples/hello-world.c
+++ examples/hello-world.c
@@ -1,6 +1,8 @@
-#!cci -std=c11 -Wall -pedantic-errors
+#!/usr/bin/env cci
+# cci: -std=c11 -Wall -pedantic-errors
+
 #include <stdio.h>
 int main() {
-    printf("Hello, world!");
+    printf("Hello, world!\n");
     return 0;
 }
