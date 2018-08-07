3a49fc8ebd3221a1ae106a5a797f5416a0a06f70
Ellen Marie Dash <me@duckie.co>
2018-08-07 19:33:28 -0400

    Update setup.cfg and setup.py.

--- setup.py
+++ setup.py
@@ -2,15 +2,5 @@
 
 import setuptools
 
-setuptools.setup(
-    entry_points={
-        "console_scripts": [
-            "cci = cci:main",
-        ],
-
-        "distutils.commands": [
-            "release = distutils_twine:release",
-        ],
-    },
-)
-
+# See setup.cfg for the actual configuration.
+setuptools.setup()
